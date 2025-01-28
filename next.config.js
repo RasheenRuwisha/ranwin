f34b1dcfb955c8f22f3a00946aaaebe27e136d2a
RRC <55465521+appy-blockchain@users.noreply.github.com>
2025-01-28 18:43:24 +0000

    New: added landing page

---
+++ next.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  webpack5: true,
+
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yzvovutt5syi9lsy.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
