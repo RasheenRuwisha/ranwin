409447439ac48bca2da3558335f6817a925c6a04
RRC <55465521+appy-blockchain@users.noreply.github.com>
2025-03-03 18:14:57 +0000

    New: Added sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://www.ranwintours.com/",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
